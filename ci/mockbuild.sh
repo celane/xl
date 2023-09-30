e56b5e0662db8f30ce5527f3e364fc637abdda9c
Charles Lane <lane@dchooz.org>
2023-09-30 02:28:38 -0400

    fix build?

--- ci/mockbuild.sh
+++ ci/mockbuild.sh
@@ -11,14 +11,16 @@ tar -xf ${NAME}-${VERSION}.tar.gz ${NAME}-${VERSION}/${NAME}.spec
 RELEASE=$(grep 'Release: ' ${NAME}-${VERSION}/${NAME}.spec | cut -d ':' -f2 | awk -F'%' '{print $1}' | tr -d ' ')
 echo RELEASE=${RELEASE} >> $GITHUB_ENV
 
-mock -r rocky+epel-8-x86_64 --rebuild --spec=${NAME}-${VERSION}/${NAME}.spec --sources=.
-mv /var/lib/mock/rocky+epel-8-x86_64/result/${NAME}-${VERSION}-${RELEASE}.el8.x86_64.rpm .
+mock -r rocky+epel-8-x86_64 --buildsrpm --spec=${NAME}-${VERSION}/${NAME}.spec --sources=.
 mv /var/lib/mock/rocky+epel-8-x86_64/result/${NAME}-${VERSION}-${RELEASE}.el8.src.rpm .
+mock -r rocky+epel-8-x86_64 --rebuild ${NAME}-${VERSION}-${RELEASE}.el8.src.rpm
+mv /var/lib/mock/rocky+epel-8-x86_64/result/${NAME}-${VERSION}-${RELEASE}.el8.x86_64.rpm .
 
-mock -r centos+epel-7-x86_64 --rebuild --spec=${NAME}-${VERSION}/${NAME}.spec --sources=.
+exit 0
+mock -r centos+epel-7-x86_64 --buildsrpm --spec=${NAME}-${VERSION}/${NAME}.spec --sources=.
 mv /var/lib/mock/centos+epel-7-x86_64/result/${NAME}-${VERSION}-${RELEASE}.el7.x86_64.rpm .
 mv /var/lib/mock/centos+epel-7-x86_64/result/${NAME}-${VERSION}-${RELEASE}.el7.src.rpm .
 
-mock -r opensuse-leap-15.3-x86_64 --rebuild --spec=${NAME}-${VERSION}/${NAME}.spec --sources=.
+mock -r opensuse-leap-15.3-x86_64 --buildsrpm --spec=${NAME}-${VERSION}/${NAME}.spec --sources=.
 mv /var/lib/mock/opensuse-leap-15.3-x86_64/result/${NAME}-${VERSION}-${RELEASE}.suse.lp153.x86_64.rpm .
 mv /var/lib/mock/opensuse-leap-15.3-x86_64/result/${NAME}-${VERSION}-${RELEASE}.suse.lp153.src.rpm .
